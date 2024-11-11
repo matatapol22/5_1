6e684abdbf8e15002f56a9dd0733892922c944b0
matatapol22 <matatapol22@gmail.com>
2024-11-11 23:30:07 +0700

    Обработка кнопки "OK"

--- app/src/main/java/com/example/a5_1/MainActivity.kt
+++ app/src/main/java/com/example/a5_1/MainActivity.kt
@@ -17,5 +17,28 @@ class MainActivity : AppCompatActivity() {
         val formatRadioGroup: RadioGroup = findViewById(R.id.formatRadioGroup)
         val photoCountEditText: EditText = findViewById(R.id.photoCountEditText)
         val calculateButton: Button = findViewById(R.id.calculateButton)
+
+        calculateButton.setOnClickListener {
+            // Получаем выбранную стоимость формата
+            val selectedFormatId = formatRadioGroup.checkedRadioButtonId
+            if (selectedFormatId == -1) {
+                Toast.makeText(this, "Выберите формат фотографии", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val selectedRadioButton = findViewById<RadioButton>(selectedFormatId)
+            val formatCost = selectedRadioButton.tag.toString().toInt()
+
+            // Получаем количество фотографий
+            val photoCount = photoCountEditText.text.toString().toIntOrNull()
+            if (photoCount == null || photoCount <= 0) {
+                Toast.makeText(this, "Введите количество фотографий", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Расчет стоимости
+            val totalCost = formatCost * photoCount
+
+
+        }
     }
 }
