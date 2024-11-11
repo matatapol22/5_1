4ef2ee1d8b1986bedc2cf4e6002479fdd71dc5da
matatapol22 <matatapol22@gmail.com>
2024-11-11 23:40:27 +0700

    Получение переданной суммы и ее отображение

--- app/src/main/java/com/example/a5_1/PaymentActivity.kt
+++ app/src/main/java/com/example/a5_1/PaymentActivity.kt
@@ -1,4 +1,19 @@
-package com.example.a5_1;
+package com.example.a5_1
 
-public class PaymentActivity {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+        val paymentTextView: TextView = findViewById(R.id.paymentTextView)
+
+        // Получение переданной стоимости
+        val totalCost = intent.getIntExtra("TOTAL_COST", 0)
+        paymentTextView.text = "Оплатить: $totalCost рублей"
+    }
 }
